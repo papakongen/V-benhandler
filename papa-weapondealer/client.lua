64aa971ec03ee71b6d9f161971be0f182b337cb8
papakongen <61790244+papakongen@users.noreply.github.com>
2021-04-09 12:49:34 +0200

    Update client.lua

--- papa-weapondealer/client.lua
+++ papa-weapondealer/client.lua
@@ -13,51 +13,89 @@ local searchingDrugs = false
 local rewardTime = math.random(4,7)
 
 --pistol
-
-Citizen.CreateThread(function ()
+Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(1)
-        local spiller = PlayerPedId()
-        if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), -430.5947265625,90.687477111816,64.263244628906, true ) < 3.0 then
-            DrawText3Ds(-430.5947265625,90.687477111816,64.263244628906, "Tryk [~g~E~w~] for at købe våben kontrakt")
-                if (IsControlJustReleased(1, 38)) then
-                TriggerServerEvent('weapondealer:Weaponbuy')
-            end       	
+        inRange = false
+ 
+        local Player = GetPlayerPed(-1)
+        local Position = GetEntityCoords(Player)
+ 
+            local dist = GetDistanceBetweenCoords(Position, -430.5947265625,90.687477111816,64.263244628906)
+            
+            if dist < 2 then
+                inRange = true
+            
+                DrawText3D(-430.5947265625,90.687477111816,64.263244628906, 'Tryk [~g~E~w~] for at købe våben kontrakt')
+                if IsControlJustReleased(0, 38) then
+                    TriggerServerEvent('weapondealer:Weaponbuy')
+            end
         end
+ 
+        if not inRange then
+            Citizen.Wait(1000)
+        end
+ 
+        Citizen.Wait(5)
     end
 end)
 
-Citizen.CreateThread(function ()
+Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(1)
-        local spiller = PlayerPedId()
-        if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), 708.54638671875,-310.10708618164,59.246913909912, true ) < 3.0 then
-            DrawText3Ds(708.54638671875,-310.10708618164,59.246913909912, "Tryk [~g~E~w~] for at købe ammunition kontrakt")
-                if (IsControlJustReleased(1, 38)) then
-                TriggerServerEvent('weapondealer:Bulletbuy')
-            end       	
+        inRange = false
+ 
+        local Player = GetPlayerPed(-1)
+        local Position = GetEntityCoords(Player)
+ 
+            local dist = GetDistanceBetweenCoords(Position, 708.54638671875,-310.10708618164,59.246913909912)
+            
+            if dist < 2 then
+                inRange = true
+            
+                DrawText3D(708.54638671875,-310.10708618164,59.246913909912, 'Tryk [~g~E~w~] for at købe ammunition kontrakt')
+                if IsControlJustReleased(0, 38) then
+                    TriggerServerEvent('weapondealer:Bulletbuy')
+            end
         end
+ 
+        if not inRange then
+            Citizen.Wait(1000)
+        end
+ 
+        Citizen.Wait(5)
     end
 end)
 
-Citizen.CreateThread(function () 
-    while true do 
-        Citizen.Wait(1) 
-        local spiller = PlayerPedId() 
-        if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), 319.50421142578,2883.2502441406,46.382823944092) < 0.7 then 
-            DrawText3Ds(319.50421142578,2883.2502441406,46.382823944092, "Tryk [~g~E~s~] for at lave en pistol")
-            if (IsControlJustReleased(1, 38)) then 
-                exports['progressBars']:startUI(2000, "Laver pistol.")
+Citizen.CreateThread(function()
+    while true do
+        inRange = false
+ 
+        local Player = GetPlayerPed(-1)
+        local Position = GetEntityCoords(Player)
+ 
+            local dist = GetDistanceBetweenCoords(Position, 319.50421142578,2883.2502441406,46.382823944092)
+            
+            if dist < 2 then
+                inRange = true
+            
+                DrawText3D(319.50421142578,2883.2502441406,46.382823944092, 'Tryk [~g~E~s~] for at lave en pistol')
+                if IsControlJustReleased(0, 38) then
+                    exports['progressBars']:startUI(2000, "Laver pistol.")
                 FreezeEntityPosition(PlayerPedId(), true) 
                 RequestAnimDict("amb@prop_human_bum_bin@idle_b") 
                 while (not HasAnimDictLoaded("amb@prop_human_bum_bin@idle_b")) do Citizen.Wait(0) end 
-                  TaskPlayAnim(spiller,"amb@prop_human_bum_bin@idle_b","idle_d",100.0, 200.0, 0.3, 120, 0.2, 0, 0, 0) 
+                  TaskPlayAnim(Player,"amb@prop_human_bum_bin@idle_b","idle_d",100.0, 200.0, 0.3, 120, 0.2, 0, 0, 0) 
                   Citizen.Wait(2000)
-                  StopAnimTask(spiller, "amb@prop_human_bum_bin@idle_b","idle_d", 1.0) 
+                  StopAnimTask(Player, "amb@prop_human_bum_bin@idle_b","idle_d", 1.0) 
                   FreezeEntityPosition(PlayerPedId(), false)
                 TriggerServerEvent('weapondealer:make') 
             end
         end
+ 
+        if not inRange then
+            Citizen.Wait(1000)
+        end
+ 
+        Citizen.Wait(5)
     end
 end)
 
@@ -78,10 +116,18 @@ end)
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(-1)
-        if drugs then
-            if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), Config.WeaponCoords[num].x, Config.WeaponCoords[num].y, Config.WeaponCoords[num].z) < 1.0 then
-                DrawText3Ds(Config.WeaponCoords[num].x, Config.WeaponCoords[num].y, Config.WeaponCoords[num].z, "Tryk på [~r~E~w~] for at åbne kassen")
+        inRange = false
+ 
+        local Player = GetPlayerPed(-1)
+        local Position = GetEntityCoords(Player)
+ 
+            local dist = GetDistanceBetweenCoords(Position, Config.WeaponCoords[num].x, Config.WeaponCoords[num].y, Config.WeaponCoords[num].z)
+            
+            if dist < 2 then
+                inRange = true
+                if drugs then
+            
+                DrawText3D(Config.WeaponCoords[num].x, Config.WeaponCoords[num].y, Config.WeaponCoords[num].z, 'Tryk på [~r~E~w~] for at åbne kassen')
                 if IsControlJustReleased(0, 38) then
                     drugPos = ""
                     TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_PARKING_METER", 0, true)
@@ -97,6 +143,12 @@ Citizen.CreateThread(function()
                 end
             end
         end
+ 
+        if not inRange then
+            Citizen.Wait(1000)
+        end
+ 
+        Citizen.Wait(5)
     end
 end)
 
@@ -117,10 +169,18 @@ end)
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(-1)
-        if drugs then
-            if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), Config.ammunitionCoords[num].x, Config.ammunitionCoords[num].y, Config.ammunitionCoords[num].z) < 1.0 then
-                DrawText3Ds(Config.ammunitionCoords[num].x, Config.ammunitionCoords[num].y, Config.ammunitionCoords[num].z, "Tryk på [~r~E~w~] for at åbne ammunition kassen")
+        inRange = false
+ 
+        local Player = GetPlayerPed(-1)
+        local Position = GetEntityCoords(Player)
+ 
+            local dist = GetDistanceBetweenCoords(Position, Config.ammunitionCoords[num].x, Config.ammunitionCoords[num].y, Config.ammunitionCoords[num].z)
+            
+            if dist < 2 then
+                inRange = true
+                if drugs then
+            
+                DrawText3D(Config.ammunitionCoords[num].x, Config.ammunitionCoords[num].y, Config.ammunitionCoords[num].z, 'Tryk på [~r~E~w~] for at åbne ammunition kassen')
                 if IsControlJustReleased(0, 38) then
                     drugPos = ""
                     TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_PARKING_METER", 0, true)
@@ -136,6 +196,12 @@ Citizen.CreateThread(function()
                 end
             end
         end
+ 
+        if not inRange then
+            Citizen.Wait(1000)
+        end
+ 
+        Citizen.Wait(5)
     end
 end)
 
@@ -161,25 +227,37 @@ end)
 
 
 --tommy
-
-Citizen.CreateThread(function () 
-    while true do 
-        Citizen.Wait(1) 
-        local spiller = PlayerPedId() 
-        if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), 508.60101318359,3100.3171386719,41.307903289795) < 0.7 then 
-            DrawText3Ds(508.60101318359,3100.3171386719,41.307903289795, "Tryk [~g~E~s~] for at lave en tommy gun")
-            if (IsControlJustReleased(1, 38)) then 
-                exports['progressBars']:startUI(35000, "Laver en tommy gun.")
-                FreezeEntityPosition(PlayerPedId(), true) 
-                RequestAnimDict("amb@prop_human_bum_bin@idle_b") 
-                while (not HasAnimDictLoaded("amb@prop_human_bum_bin@idle_b")) do Citizen.Wait(0) end 
-                  TaskPlayAnim(spiller,"amb@prop_human_bum_bin@idle_b","idle_d",100.0, 200.0, 0.3, 120, 0.2, 0, 0, 0) 
-                  Citizen.Wait(35000)
-                  StopAnimTask(spiller, "amb@prop_human_bum_bin@idle_b","idle_d", 1.0) 
-                  FreezeEntityPosition(PlayerPedId(), false)
-                TriggerServerEvent('tommydealer:make') 
+Citizen.CreateThread(function()
+    while true do
+        inRange = false
+ 
+        local Player = GetPlayerPed(-1)
+        local Position = GetEntityCoords(Player)
+ 
+            local dist = GetDistanceBetweenCoords(Position, -430.5947265625,90.687477111816,64.263244628906)
+            
+            if dist < 2 then
+                inRange = true
+            
+                DrawText3D(-430.5947265625,90.687477111816,64.263244628906, 'Tryk [~g~E~s~] for at lave en tommy gun')
+                if IsControlJustReleased(0, 38) then
+                    exports['progressBars']:startUI(35000, "Laver en tommy gun.")
+                    FreezeEntityPosition(PlayerPedId(), true) 
+                    RequestAnimDict("amb@prop_human_bum_bin@idle_b") 
+                    while (not HasAnimDictLoaded("amb@prop_human_bum_bin@idle_b")) do Citizen.Wait(0) end 
+                      TaskPlayAnim(Player,"amb@prop_human_bum_bin@idle_b","idle_d",100.0, 200.0, 0.3, 120, 0.2, 0, 0, 0) 
+                      Citizen.Wait(35000)
+                      StopAnimTask(Player, "amb@prop_human_bum_bin@idle_b","idle_d", 1.0) 
+                      FreezeEntityPosition(PlayerPedId(), false)
+                    TriggerServerEvent('tommydealer:make') 
             end
         end
+ 
+        if not inRange then
+            Citizen.Wait(1000)
+        end
+ 
+        Citizen.Wait(5)
     end
 end)
 
